3a7e4ff48350b39f84fce17bd465e18dea934890
bolichep <bolichep@gmail.com>
2014-05-05 02:13:04 -0300

    Masive change.

--- wrad.py
+++ wrad.py
@@ -1,148 +1,148 @@
 #!/usr/bin/python -tt
 
-from gi.repository import Gtk
-import pygst
-#pygst.require("0.10")
+import gtk
 import gst
-#import pygtk
-import sys
 
-#INCR = 1000000000
-INCR = 200000000
-DELAY = 0
-REGDELAY = 0
-class  WradWindow(Gtk.Window):
-	def __init__(self):
-		Gtk.Window.__init__(self, title="Audio Delay")
+Incr = 200000000
+Delay = 0
+#RegDelay = 0
+#label0 = None
 
-		self.set_size_request(350, 35)
-		self.set_position(Gtk.WindowPosition.CENTER)
+def on_buttonX_clicked(widget):
+    return
 
-		self.box = Gtk.Box(spacing=6 , orientation = Gtk.Orientation.HORIZONTAL)
-		self.add(self.box)
-
-		self.button0 = Gtk.Button(label="Start") # Boton Start/Stop
-		self.button1 = Gtk.Button(label="Inc")
-		self.button2 = Gtk.Button(label="Dec")
-		self.button3 = Gtk.Button(label="Reset")
-		self.button4 = Gtk.Button(label="Save")
-		self.button5 = Gtk.Button(label="Restore")
-
-		self.button0.connect("clicked", self.on_buttonSTART_clicked)
-		self.button1.connect("clicked", self.on_buttonINC_clicked)
-		self.button2.connect("clicked", self.on_buttonDEC_clicked)
-		self.button3.connect("clicked", self.on_buttonRST_clicked)
-		self.button4.connect("clicked", self.on_buttonSAV_clicked)
-		self.button5.connect("clicked", self.on_buttonRES_clicked)
+def abutton(abox="", label="",event="",callback=on_buttonX_clicked):
+    buttonX = gtk.Button(label)
+    buttonX.connect("clicked", callback)
+    abox.pack_start(buttonX, True, True, 0)
+    return buttonX
+    
 
 
-		self.box.pack_start(self.button2, True, True, 0 )
-		self.box.pack_start(self.button0, True, True, 0 )
-		self.box.pack_start(self.button1, True, True, 0 )
+def Window():
+    global button1
+    global label0
 
-		self.label = Gtk.Label() # Current Delay Label
-		self.label.set_text("Delay")
-		self.box.pack_start(self.label, True, True, 0 )	
-		
-		self.box.pack_start(self.button3, True, True, 0 )				
+    window = gtk.Window()
+    window.set_default_size(350,35)
+    window.set_position(gtk.WIN_POS_CENTER)
+    window.connect("destroy", lambda w: gtk.main_quit())
 
-		self.box.pack_start(self.button4, True, True, 0 )
-		self.box.pack_start(self.button5, True, True, 0 )
 
-		self.QueueRun()
+    box = gtk.HBox(spacing=6)
+    window.add(box)
 
-	def QueueRun(self):
-		self.delay_pipeline = gst.Pipeline("mypipeline")
-		self.delay_pipeline.set_property("auto-flush-bus", True)
-		#ALSA
-		self.audiosrc = gst.element_factory_make("alsasrc", "audio")
-		self.audiosrc.set_property("device","default")
-		self.delay_pipeline.add(self.audiosrc)
-		#Queue
-		self.audioqueue = gst.element_factory_make("queue","queue1")
-		self.audioqueue.set_property("max-size-time",0)
-		self.audioqueue.set_property("max-size-buffers",0)
-		self.audioqueue.set_property("max-size-bytes",0)
-		self.audioqueue.set_property("min-threshold-time",DELAY)
-		self.audioqueue.set_property("leaky","no")
-		self.delay_pipeline.add(self.audioqueue)
-		#Audio Output
-		self.sink = gst.element_factory_make("autoaudiosink", "sink")
-		self.delay_pipeline.add(self.sink)
-		#Link the elements
-		self.audiosrc.link(self.audioqueue)
-		self.audioqueue.link(self.sink)
-		#Begin Playing
-		#self.delay_pipeline.set_state(gst.STATE_PLAYING)
-		
-	def changeDelay(self):
-		self.label.set_text(str(float(DELAY/float(1000000000))))			
-		self.delay_pipeline.set_state(gst.STATE_NULL)
-		self.audioqueue.set_property("min-threshold-time",DELAY)		
-		self.delay_pipeline.set_state(gst.STATE_PLAYING)
+    button0 = abutton(abox=box,label="Dec",event="clicked",callback=on_buttonDec_clicked)
+    button1 = abutton(abox=box,label="Start",event="clicked",callback=on_buttonStart_clicked)
+    button2 = abutton(abox=box,label="Inc",event="clicked",callback=on_buttonInc_clicked)
+    label0 = gtk.Label()
+    label0.set_text("Delay")
+    box.pack_start(label0, True, True, 0)
+    button3 = abutton(abox=box,label="Reset",event="clicked",callback=on_buttonReset_clicked)
+    button4 = abutton(abox=box,label="Save",event="clicked",callback=on_buttonSave_clicked)
+    button5 = abutton(abox=box,label="Restore",event="clicked",callback=on_buttonRestore_clicked)
 
-	# Stop
-	def on_buttonSTOP_clicked(self, widget):
-		self.button0.set_label("Start")
-		self.button0.connect("clicked", self.on_buttonSTART_clicked)
-		self.delay_pipeline.set_state(gst.STATE_NULL)
-		
-	# Start Delay
-	def on_buttonSTART_clicked(self, widget):
-		global DELAY
-		self.changeDelay()			
-		self.button0.set_label("Stop")
-		self.button0.connect("clicked", self.on_buttonSTOP_clicked)
-	
-	# Incrementa Delay		
-	def on_buttonINC_clicked(self, widget):
-		global DELAY
-		DELAY = DELAY + INCR
-		#print "Inc DELAY", DELAY
-		self.changeDelay()	
-		self.button0.set_label("Stop")
-		self.button0.connect("clicked", self.on_buttonSTOP_clicked)
-			
-	# Decrementa Delay		
-	def on_buttonDEC_clicked(self, widget):
-		global DELAY
-		DELAY = DELAY - INCR
-		if (DELAY < 0):
-			DELAY = 0
-		#print "Dec DELAY", DELAY
-		self.changeDelay()
-		self.button0.set_label("Stop")
-		self.button0.connect("clicked", self.on_buttonSTOP_clicked)
+    window.show_all()
+    QueueRun()
 
-	# Reset Delay
-	def on_buttonRST_clicked(self, widget):
-		global DELAY
-		DELAY = 0
-		#print "Reset ", DELAY
-		self.changeDelay()
-		self.button0.set_label("Stop")
-		self.button0.connect("clicked", self.on_buttonSTOP_clicked)
+def QueueRun():
+    global delay_pipeline
+    global audioqueue
 
-	# Save current delay
-	def on_buttonSAV_clicked(self,widget):
-		global DELAY
-		global REGDELAY
-		REGDELAY = DELAY
+    delay_pipeline = gst.Pipeline("pipeline")
+    delay_pipeline.set_property("auto-flush-bus", False)
+    #ALSA
+    audiosrc = gst.element_factory_make("alsasrc", "audio")
+    audiosrc.set_property("device","default")
+    delay_pipeline.add(audiosrc)
+    #Queue
+    audioqueue = gst.element_factory_make("queue","queue1")
+    audioqueue.set_property("max-size-time",0)
+    audioqueue.set_property("max-size-buffers",0)
+    audioqueue.set_property("max-size-bytes",0)
+    audioqueue.set_property("min-threshold-time",Delay)
+    audioqueue.set_property("leaky","no")
+    delay_pipeline.add(audioqueue)
+    #Audio Output
+    sink = gst.element_factory_make("autoaudiosink", "sink")
+    delay_pipeline.add(sink)
+    #Link the elements
+    audiosrc.link(audioqueue)
+    audioqueue.link(sink)
+    #Begin Playing
+    # delay_pipeline.set_state(gst.STATE_PLAYING)
 
-	# Restore current delay
-	def on_buttonRES_clicked(self,widget):
-		global DELAY
-		global REGDELAY
-		DELAY = REGDELAY
-		self.changeDelay()
+def changeDelay():
+    global label0
 
-window = WradWindow()
-window.connect("destroy", lambda w: Gtk.main_quit())
+    label0.set_text(str(float(Delay/float(1000000000))))			
+    delay_pipeline.set_state(gst.STATE_NULL)
+    audioqueue.set_property("min-threshold-time",Delay)		
+    delay_pipeline.set_state(gst.STATE_PLAYING)
 
-buttonx = Gtk.Button(label="Exit");
-buttonx.connect("clicked", Gtk.main_quit)
-window.box.pack_end(buttonx,True,True,0)
 
-window.show_all()
+def on_buttonStart_clicked(widget):
+    global button1
+    changeDelay()			
+    button1.set_label("Stop")
+    button1.connect("clicked", on_buttonStop_clicked)
 
-Gtk.main()
+def on_buttonStop_clicked(widget):		
+    global button1
+    button1.set_label("Start")
+    button1.connect("clicked", on_buttonStart_clicked)
+    delay_pipeline.set_state(gst.STATE_NULL)
+    return
+
+def on_buttonDec_clicked(widget):		
+    global Delay
+    global button1
+    Delay = Delay - Incr
+    if (Delay < 0):
+        Delay = 0
+        #print "Dec DELAY", DELAY
+        button1.set_label("Stop")
+        button1.connect("clicked", on_buttonStop_clicked)
+    changeDelay()
+    return
+
+def on_buttonInc_clicked(widget):
+    global Delay
+    global button1
+    Delay = Delay + Incr
+    #print "Inc DELAY", DELAY
+    changeDelay()	
+    button1.set_label("Stop")
+    button1.connect("clicked", on_buttonStop_clicked)
+    return
+
+def on_buttonSave_clicked(widget):
+    global Regdelay
+    Regdelay = Delay
+    changeDelay()
+    return
+
+def on_buttonReset_clicked(widget):
+    global Delay
+    global button1
+    Delay = 0
+    changeDelay()
+    button1.set_label("Stop")
+    button1.connect("clicked", on_buttonStop_clicked)
+    return
+
+def on_buttonRestore_clicked(widget):
+    global Delay
+    Delay = Regdelay
+    changeDelay()
+    return
+
+    
+def main():
+    Window()
+    gtk.main()
+
+if __name__ == '__main__':
+  main()
+    
+    
